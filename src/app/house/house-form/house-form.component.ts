b081f03bd76d8efa98f918d17d1f15ad25e5294f
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-08-05 21:13:01 -0300

    feat: update house form to create and update houses

--- src/app/house/house-form/house-form.component.ts
+++ src/app/house/house-form/house-form.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-import { HOUSES } from '../../shared/db';
+import { DB } from '../../shared/db';
 import { House } from '../../shared/models/House';
 
 @Component({
@@ -10,12 +11,25 @@ import { House } from '../../shared/models/House';
   styleUrl: './house-form.component.css'
 })
 export class HouseFormComponent {
+  public id: number = 0;
   public houseName: string = ''
   public size: string = ''
   public rooms: string = ''
   public bathrooms: string = ''
 
-  constructor(private _snackBar: MatSnackBar) {}
+  constructor(private _snackBar: MatSnackBar, private _route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.id = Number(this._route.snapshot.paramMap.get('id'));
+
+    if (this.id !== 0) {
+      const house = DB.getHouse(this.id);
+      this.houseName = house.houseName;
+      this.size = house.sizeInSquareMeter.toString();
+      this.rooms = house.rooms.toString();
+      this.bathrooms = house.bathrooms.toString();
+    }
+  }
 
   public registerNewHouse(): void {
     const newHouse = new House(
@@ -25,22 +39,37 @@ export class HouseFormComponent {
       Number(this.bathrooms)
     )
 
-    HOUSES.push(newHouse);
+    DB.addHouse(newHouse);
 
     this.clearInputs();
-    this.openSnackBar();
+    this.openSnackBar('Casa cadastrada com sucesso!');
   }
 
-  private openSnackBar(): void {
-    this._snackBar.open('Casa cadastrada com sucesso!', 'Ok', {
+  public editHouse(): void {
+    const house = new House(
+      this.houseName,
+      Number(this.size),
+      Number(this.rooms),
+      Number(this.bathrooms)
+    );
+
+    house.id = this.id;
+
+    DB.updateHouse(house);
+    this.clearInputs();
+    this.openSnackBar('Casa editada com sucesso!');
+  }
+
+  private openSnackBar(message: string): void {
+    this._snackBar.open(message, 'Ok', {
       duration: 3000
     });
   }
 
   private clearInputs(): void {
-    this.houseName = ''
-    this.size = ''
-    this.rooms = ''
-    this.bathrooms = ''
+    this.houseName = '';
+    this.size = '';
+    this.rooms = '';
+    this.bathrooms = '';
   }
 }
